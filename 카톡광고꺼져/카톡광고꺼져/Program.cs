c112d1d3f8f895c5c137cc432b351c2c0cad0a70
asdf-2345 <52937308+asdf-2345@users.noreply.github.com>
2020-01-21 13:44:51 +0900

    광고 잘리고 남은 하얀 부분 없엠
    근데 위에도 잘리는 버그생김 ㅋㅋ

--- 카톡광고꺼져/카톡광고꺼져/Program.cs
+++ 카톡광고꺼져/카톡광고꺼져/Program.cs
@@ -1,13 +1,7 @@
-﻿/*
- * Created by SharpDevelop.
- * User: Administrator
- * Date: 2020-01-21
- * Time: 오전 11:57
- * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
- */
 using System;
 using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.Drawing;
 namespace 카톡광고꺼져
 {
 	class Program
@@ -22,14 +16,30 @@ namespace 카톡광고꺼져
 		public static extern Boolean ShowWindow(IntPtr hWnd, Int32 nCmdShow);
 
 		[DllImport("user32")]
-		public static extern int SetWindowPos(int hwnd, int hWndInsertAfter, int x, int y, int cx, int cy, int wFlags);
-		                  
-		[DllImport("user32")]
-		public static extern int GetWindowRect(int hwnd, ref RECT lpRect);
-			
+		public static extern int SetWindowPos(IntPtr hwnd, int hWndInsertAfter, int x, int y, int cx, int cy, int wFlags);
+		  
+	 	[DllImport("user32.dll")]
+       	 	static extern bool GetWindowRect(int hWnd, out RECT lpRect);
+
 		public static void Main(string[] args)
 		{
-			ShowWindow(FindWindowEx(FindWindow(null, "카카오톡"), 0, "EVA_Window", null), 0);
+			int katalk = FindWindow(null, "카카오톡");
+			IntPtr ADS = FindWindowEx(katalk, 0, "EVA_Window", null);
+			ShowWindow(ADS, 0);
+			RECT rect;
+ 			GetWindowRect(katalk, out rect);
+			int x = (rect.Right - rect.Left);
+			int y = (rect.Bottom - rect.Top);
+			IntPtr katalk2 = FindWindowEx(katalk, 0, "EVA_ChildWindow", null);
+			SetWindowPos(katalk2, 0, 0, 0, x, y, 0);
 		}
+		
+		public struct RECT
+        	{
+            		public int Left;       
+            		public int Top;         
+            		public int Right;      
+            		public int Bottom;
+        	}
 	}
 }
